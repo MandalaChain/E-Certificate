ed6c6a23d38dbe9f8a779ea286fa06cdf189767e
anggadanarp <anggadanarp@gmail.com>
2024-10-14 21:22:54 +0700

    feat: setup for deploy in localhost

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -83,6 +83,13 @@ const config: HardhatUserConfig = {
     },
   },
   networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 1337,
+    },
     truffle: {
       url: 'http://localhost:24012/rpc',
       timeout: 60000,
